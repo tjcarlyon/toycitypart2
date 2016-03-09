3edba0cd34f8092cc6be6a6d6ae5332da3c49de9
tjcarlyon <tjcarlyon@gmail.com>
2016-03-08 19:19:47 -0600

    brands

--- app.rb
+++ app.rb
@@ -1,8 +1,6 @@
 require 'json'
 require 'date'
-
-
-#methods
+# methods go here
 
 def start
   setup_files # load, read, parse, and create the files
@@ -17,15 +15,15 @@ def setup_files #method to open the JSON file and parse the data
 end
 
 def create_report # start report generation
-  sales_report_header #we didnt cover the gems section yet
+  sales_header
   print_date
-  products_header #we didnt cover the gems section yet
-  parse_products #pulls out names/ites in string and re-assigns
-  #brands_report_header  #pulls out names/ites in string and re-assigns
-  #parse_brands
+  products_header
+  parse_products
+  brands_report_header
+  parse_brands
 end
 
-def sales_report_header # Print "Sales Report" in ascii art
+def sales_header # Print "Sales Report" in ascii art
   $report_file.puts "  #####                                 ######                            "
   $report_file.puts " #     #   ##   #      ######  ####     #     # ###### #####   ####  #####  #####"
   $report_file.puts " #        #  #  #      #      #         #     # #      #    # #    # #    #   # "
@@ -34,16 +32,23 @@ def sales_report_header # Print "Sales Report" in ascii art
   $report_file.puts " #     # #    # #      #      #    #    #    #  #      #      #    # #   #    #  "
   $report_file.puts "  #####  #    # ###### ######  ####     #     # ###### #       ####  #    #   # "
   star_string_divider
+  star_string_divider
+  star_string_divider
+  $report_file.puts " "
+  $report_file.puts " "
+  $report_file.puts " "
+  $report_file.puts " "
+  
 end
 
 def star_string_divider
   $report_file.puts "********************************************************************************"
 end
 
-# def print_date # Print today's date
-  #time = Time.new
-  #$report_file.puts time.strftime("Current date: %m/%d/%Y")
-#end
+def print_date # Print today's date
+  time = Time.new
+  $report_file.puts time.strftime("Current date: %m/%d/%Y")
+end
 
 def products_header # Print "Products" in ascii art
   $report_file.puts "                     _            _       "
@@ -68,27 +73,51 @@ def print_item_data(toy)
   $report_file.puts "Toy title: #{toy["title"]}" # Print the name of the toy
   $report_file.puts "Retail price: #{toy["full-price"]}" # Print the retail price of the toy
   $report_file.puts "Total number of purchases: #{toy["purchases"].length}"   # Calculate and print the total number of purchases
-  $report_file.puts "Total sales: #{total_sales_products(toy)}" # Calculate and print the total amount of sales
-  $report_file.puts "Average price: #{total_sales_products(toy)/toy["purchases"].length}" # Calculate and print the average price the toy sold for
-  $report_file.puts "Average discount: #{toy["full-price"].to_f-(total_sales_products(toy)/toy["purchases"].length)}" # Calculate and print the average discount (% or $) based off the average sales price
+  $report_file.puts "Total sales: #{calc_total_sales_products(toy)}" # Calculate and print the total amount of sales
+  $report_file.puts "Average price: #{calc_total_sales_products(toy)/toy["purchases"].length}" # Calculate and print the average price the toy sold for
+  $report_file.puts "Average discount: #{toy["full-price"].to_f-(calc_total_sales_products(toy)/toy["purchases"].length)}" # Calculate and print the average discount (% or $) based off the average sales price
 end
 
-def total_sales_products(toy)
+def calc_total_sales_products(toy)
   total_sales = 0
   toy["purchases"].each { |purchase| total_sales = total_sales + purchase["price"]}
   return total_sales
 end
 
-# Print "Brands" in ascii art
-# For each brand in the data set:
-# Print the name of the brand
-# Calculate and print the total sales volume of all the brand's toys combined
-# Calculate and print the total sales volume of all the brand's toys 
-# Calculate and print the average price of the brand's toys
-# Count and print the number of the brand's toys we stock
+def brands_report_header # Print "Brands" in ascii art
+  $report_file.puts " _                         _     "
+  $report_file.puts "| |                       | |    "
+  $report_file.puts "| |__  _ __ __ _ _ __   __| |___ "
+  $report_file.puts "| '_ \\| '__/ _` | '_ \\ / _` / __|"
+  $report_file.puts "| |_) | | | (_| | | | | (_| \\__ \\"
+  $report_file.puts "|_.__/|_|  \\__,_|_| |_|\\__,_|___/"
+  $report_file.puts
+  star_string_divider
+end
+
+def parse_brands # For each brand in the data set:
+  unique_brands = $products_hash["items"].map { |item| item["brand"] }.uniq
+  unique_brands.each_with_index do | brand, index |
+    print_brands(brand)
+  end
+end
+
+def brands_items(brand)
+  brands_items = $products_hash["items"].select { |item| item["brand"] == brand }
+  return brands_items
+end
+
+def print_brands(brand)
+  $report_file.puts "Brand: #{brand}" # Print the name of the brand
+end
   
+ # Calculate and print the total sales volume of all the brand's toys combined
+ 
+ # Calculate and print the average price of the brand's toys
 
-#start
+ # Count and print the number of the brand's toys we stock
 
 
+
+start
 
